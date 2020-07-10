571613bfb2f28abcdfb7d061ee942d1db49c6e36
Michiel Thomassen <mich.thomassen@gmail.com>
2020-07-10 13:35:56 +0200

    Data model

---
+++ SportsStore/src/app/model/model.module.ts
@@ -0,0 +1,8 @@
+import {ProductRepository} from './product.repository';
+import {StaticDatasource} from './static.datasource';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  providers: [ProductRepository, StaticDatasource]
+})
+export class ModelModule { }
