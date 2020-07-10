ef8fe818cc96f95e0332f6c22d6404d1dc06a118
Michiel Thomassen <mich.thomassen@gmail.com>
2020-07-10 14:43:49 +0200

    Starting the store

---
+++ SportsStore/src/app/store/store.component.ts
@@ -0,0 +1,19 @@
+import {ProductRepository} from '../model/product.repository';
+import {Product} from '../model/product.model';
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'store',
+  templateUrl: 'store.component.html'
+})
+export class StoreComponent {
+  constructor(private repository: ProductRepository) {  }
+
+  get products(): Product[] {
+    return this.repository.getProducts();
+  }
+
+  get categories(): string[] {
+    return this.repository.getCategories();
+  }
+}
