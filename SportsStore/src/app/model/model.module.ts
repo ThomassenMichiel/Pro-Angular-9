001b14a760023622806346f5ad89622af5e6eb5f
Michiel Thomassen <bunzzbunzz@gmail.com>
2020-07-12 23:51:28 +0200

    Finish checkout

--- SportsStore/src/app/model/model.module.ts
+++ SportsStore/src/app/model/model.module.ts
@@ -2,8 +2,10 @@ import {ProductRepository} from './product.repository';
 import {StaticDatasource} from './static.datasource';
 import {NgModule} from '@angular/core';
 import {Cart} from './cart.model';
+import {Order} from './order.model';
+import {OrderRepository} from './order.repository';
 
 @NgModule({
-  providers: [ProductRepository, StaticDatasource, Cart]
+  providers: [ProductRepository, StaticDatasource, Cart, Order, OrderRepository]
 })
 export class ModelModule { }
